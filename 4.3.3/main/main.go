98bd86195e24d53c1a9e03cd7a9ef453b761366b
Talaran <talaran.goo@gmail.com>
2015-07-09 08:49:01 +0200

    done

---
+++ 4.3.3/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+var names = []string{"Katrina", "Evan", "Neil", "Adam", "Martin", "Matt",
+	"Emma", "Isabella", "Emily", "Madison",
+	"Ava", "Olivia", "Sophia", "Abigail",
+	"Elizabeth", "Chloe", "Samantha",
+	"Addison", "Natalie", "Mia", "Alexis"}
+
+
+func main() {
+
+	var max int
+
+	for _, value := range names {
+		len := utf8.RuneCountInString(value)
+		if len > max {
+			max = len
+		}
+	}
+
+	counter := make([][]string, max)
+
+	for _, value := range names {
+		len := utf8.RuneCountInString(value)
+		counter[len-1]=append(counter[len-1], value)
+	}
+
+	fmt.Printf("%q", counter)
+
+}
