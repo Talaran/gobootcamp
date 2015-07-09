cb7a7e89886360aa710b1675a04671b88dd13cae
Talaran <talaran.goo@gmail.com>
2015-07-09 07:19:27 +0200

    Initial commit

---
+++ 3.7/main/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Id             int
+	Name, Location string
+}
+
+func (user *User) Greetings() string {
+
+	return fmt.Sprintf("Hi %s from %s", user.Name, user.Location)
+
+}
+
+type Player struct {
+	*User
+	GameId int
+}
+
+func main() {
+
+	player := &Player{&User{123, "Samuel", "China"}, 456}
+
+	println(player.Greetings())
+
+}
